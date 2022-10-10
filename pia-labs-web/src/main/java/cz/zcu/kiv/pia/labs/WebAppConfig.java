7ae1dd2eb8bc07891a6655c6ad28591217649a86
Pavel Fidransky <pavel.fidransky@yoso.fi>
2022-10-10 20:26:18 +0200

    configure Spring WebFlux to use Jackson serializer

--- pia-labs-web/src/main/java/cz/zcu/kiv/pia/labs/WebAppConfig.java
+++ pia-labs-web/src/main/java/cz/zcu/kiv/pia/labs/WebAppConfig.java
@@ -8,13 +8,16 @@ import cz.zcu.kiv.pia.labs.number.reactive.ConstantReactiveNumberService;
 import cz.zcu.kiv.pia.labs.number.reactive.RandomReactiveNumberService;
 import cz.zcu.kiv.pia.labs.number.reactive.ReactiveNumberService;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
@@ -27,6 +30,9 @@ import static org.slf4j.LoggerFactory.getLogger;
 @ComponentScan(basePackages = "cz.zcu.kiv.pia.labs")
 public class WebAppConfig implements WebFluxConfigurer {
     private static final Logger LOG = getLogger(WebAppConfig.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @EventListener
     public void onContextStarted(ContextStartedEvent cse) {
@@ -59,5 +65,11 @@ public class WebAppConfig implements WebFluxConfigurer {
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .registerModule(new JavaTimeModule());
+    }
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper));
+        configurer.defaultCodecs().jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper));
     }
 }
