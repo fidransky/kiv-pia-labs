687b0d153ab461fb6c86391e3dcb1cc8830e5373
Pavel Fidransky <pavel.fidransky@yoso.fi>
2022-10-20 20:15:36 +0200

    implement "view room" page showing sent messages and allowing users to send a new message

---
+++ pia-labs-web/src/main/java/cz/zcu/kiv/pia/labs/chat/ui/vo/MessageVO.java
@@ -0,0 +1,9 @@
+package cz.zcu.kiv.pia.labs.chat.ui.vo;
+
+/**
+ * Value object used in the form for sending messages to a chat room
+ *
+ * @param text Entered message text
+ */
+public record MessageVO(String text) {
+}
