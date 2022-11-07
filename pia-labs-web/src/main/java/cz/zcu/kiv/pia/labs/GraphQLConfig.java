c064d36934ba9bbe78e057c652c817519970e5ef
Pavel Fidransky <pavel.fidransky@yoso.fi>
2022-11-07 21:15:54 +0100

    add WebSockets support, configure GraphQL subscriptions

--- pia-labs-web/src/main/java/cz/zcu/kiv/pia/labs/GraphQLConfig.java
+++ pia-labs-web/src/main/java/cz/zcu/kiv/pia/labs/GraphQLConfig.java
@@ -1,6 +1,7 @@
 package cz.zcu.kiv.pia.labs;
 
 import graphql.scalars.ExtendedScalars;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +10,22 @@ import org.springframework.graphql.ExecutionGraphQlService;
 import org.springframework.graphql.data.method.annotation.support.AnnotatedControllerConfigurer;
 import org.springframework.graphql.execution.*;
 import org.springframework.graphql.server.WebGraphQlHandler;
+import org.springframework.graphql.server.WebGraphQlInterceptor;
 import org.springframework.graphql.server.webflux.GraphQlHttpHandler;
+import org.springframework.graphql.server.webflux.GraphQlWebSocketHandler;
 import org.springframework.graphql.server.webflux.GraphiQlHandler;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.server.support.WebSocketUpgradeHandlerPredicate;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
@@ -63,7 +74,7 @@ public class GraphQLConfig {
 
     @Bean
     public GraphiQlHandler graphiQlHandler() {
-        return new GraphiQlHandler("/graphql", "");
+        return new GraphiQlHandler("/graphql", "/graphql");
     }
 
     @Bean
@@ -71,5 +82,24 @@ public class GraphQLConfig {
         return RouterFunctions
                 .route(POST("/graphql").and(accept(MediaType.APPLICATION_JSON)), graphQlHttpHandler::handleRequest)
                 .andRoute(GET("/graphiql").and(accept(MediaType.TEXT_HTML)), graphiQlHandler::handleRequest);
+    }
+
+    @Bean
+    public WebGraphQlHandler webGraphQlHandler(ExecutionGraphQlService service, ObjectProvider<WebGraphQlInterceptor> interceptorsProvider) {
+        return WebGraphQlHandler.builder(service).interceptors(interceptorsProvider.orderedStream().collect(Collectors.toList())).build();
+    }
+
+    @Bean
+    public GraphQlWebSocketHandler graphQlWebSocketHandler(WebGraphQlHandler webGraphQlHandler, ServerCodecConfigurer configurer) {
+        return new GraphQlWebSocketHandler(webGraphQlHandler, configurer, Duration.ofSeconds(60));
+    }
+
+    @Bean
+    public HandlerMapping graphQlWebSocketEndpoint(GraphQlWebSocketHandler graphQlWebSocketHandler) {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setHandlerPredicate(new WebSocketUpgradeHandlerPredicate());
+        mapping.setUrlMap(Collections.singletonMap("/graphql", graphQlWebSocketHandler));
+        mapping.setOrder(-2);
+        return mapping;
     }
 }
