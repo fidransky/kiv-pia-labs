3b436953700aada513c36ae5e5f7ab8a92e6b894
Pavel Fidransky <pavel.fidransky@yoso.fi>
2022-11-07 21:20:15 +0100

    stream messages from message broker using STOMP via WebSocket

--- pia-labs-react-ui/src/components/ViewRoom.tsx
+++ pia-labs-react-ui/src/components/ViewRoom.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 import { APP_NAME } from "../App";
 import { getRoom, getRoomMessages } from "../services/RoomService";
 import { Message, Room } from "../types";
 import MessageListItem from "./MessageListItem";
 import SendMessageForm from "./SendMessageForm";
+import { Client, StompSubscription } from '@stomp/stompjs';
 
 export function loader({ params }: LoaderFunctionArgs): Promise<LoaderData> {
     return Promise.all([
@@ -27,13 +28,39 @@ type LoaderData = {
 type Props = {
 }
 
+const client = new Client();
+client.brokerURL = 'ws://localhost:61614/ws';
+
 export default function ViewRoom(props: Props) {
     const { room, messages } = useLoaderData() as LoaderData;
+    const [ messageStream, setMessageStream ] = useState(messages);
 
     useEffect(() => {
         if (room === null) return;
 
         document.title = APP_NAME + ' - ' + room.name;
+    }, [ room ]);
+
+    useEffect(() => {
+        let subscription: StompSubscription | undefined;
+
+        client.onConnect = function onConnect(frame) {
+            const destination = '/topic/kiv.pia.chat.room.' + room.id;
+
+            subscription = client.subscribe(destination, function onMessageReceived(payload) {
+                const message = JSON.parse(payload.body);
+                message.timestamp = new Date(message.timestamp);
+                setMessageStream((messageStream) => messageStream.concat(message));
+            });
+        };
+        client.activate();
+
+        return () => {
+            if (subscription !== undefined) {
+                subscription.unsubscribe();
+            }
+        };
+
     }, [ room ]);
 
     return (
@@ -42,7 +69,7 @@ export default function ViewRoom(props: Props) {
                 <main className="col">
                     <div className="row justify-content-md-center">
                         <div className="col-md-10 col-lg-9 col-xl-8">
-                            {messages.map((message) => <MessageListItem key={message.id} message={message}/>)}
+                            {messageStream.map((message) => <MessageListItem key={message.id} message={message}/>)}
 
                             <SendMessageForm/>
                         </div>
