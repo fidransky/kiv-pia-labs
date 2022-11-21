42da462b02a389f63f9add00b53cf5018e055819
Pavel Fidransky <pavel.fidransky@yoso.fi>
2022-11-21 22:07:50 +0100

    set JDBC connection properties in application.properties file

--- pia-labs-core/src/main/java/cz/zcu/kiv/pia/labs/BeanConfig.java
+++ pia-labs-core/src/main/java/cz/zcu/kiv/pia/labs/BeanConfig.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class BeanConfig {
@@ -18,5 +21,13 @@ public class BeanConfig {
                 .setSerializationInclusion(JsonInclude.Include.NON_NULL)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .registerModule(new JavaTimeModule());
+    }
+
+    @Bean
+    public PropertySourcesPlaceholderConfigurer applicationPropertiesConfigurer(@Value("classpath:application.properties") Resource applicationProperties) {
+        var configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocations(applicationProperties);
+        configurer.setIgnoreUnresolvablePlaceholders(true);
+        return configurer;
     }
 }
