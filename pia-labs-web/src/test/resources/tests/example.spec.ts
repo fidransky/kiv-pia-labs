91d946513114347b130b948ef9366e5175ede5d1
Pavel Fidransky <pavel.fidransky@yoso.fi>
2022-12-05 19:53:19 +0100

    implement end-to-end tests of chat rooms app

--- pia-labs-web/src/test/resources/tests/example.spec.ts
+++ pia-labs-web/src/test/resources/tests/example.spec.ts
@@ -1,20 +1,32 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has title and links to intro page', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://localhost:8080/pia-labs/spring/');
+  await expect(page).toHaveTitle('Please sign in');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  // Log in
+  await page.getByPlaceholder('Username').fill('john.doe');
+  await page.getByPlaceholder('Password').fill('password');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page).toHaveTitle('KIV/PIA Labs - Chat rooms');
 
-  // create a locator
-  const getStarted = page.getByRole('link', { name: 'Get started' });
+  const chatRoomName = 'test-' + Date.now();
 
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+  // Search test chat room
+  await page.getByPlaceholder('Search chat room by name').fill(chatRoomName);
+  await page.getByPlaceholder('Search chat room by name').press('Enter');
+  await expect(page.getByRole('alert')).toBeVisible();
+  await expect(page.getByRole('alert')).toContainText('No chat rooms were found for query ' + chatRoomName + '.');
 
-  // Click the get started link.
-  await getStarted.click();
+  // Create test chat room
+  await page.getByRole('link', { name: 'Create a new chat room' }).click();
+  await page.locator('#roomName').fill(chatRoomName);
+  await page.getByRole('button', { name: 'Create' }).click();
+  await expect(page.locator('.navbar-brand')).toContainText(chatRoomName + ' chat room');
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // Search test chat room again
+  await page.locator('.navbar-brand').click();
+  await page.getByPlaceholder('Search chat room by name').fill(chatRoomName);
+  await page.getByPlaceholder('Search chat room by name').press('Enter');
+  await expect(page.getByRole('alert')).not.toBeVisible();
 });
