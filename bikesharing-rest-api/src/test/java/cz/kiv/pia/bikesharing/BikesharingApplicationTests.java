c478c53ac9050655ed112b5f66fa02b2bcd1237a
Pavel Fidransky <pavel.fidransky@yoso.fi>
2023-10-03 13:32:34 +0200

    Add simple Spring application context integration test

---
+++ bikesharing-rest-api/src/test/java/cz/kiv/pia/bikesharing/BikesharingApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.kiv.pia.bikesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikesharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
