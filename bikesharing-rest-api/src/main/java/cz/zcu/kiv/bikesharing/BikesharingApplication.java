109a8fdb3c3075a5dc76b962fd115929f4c4bbb2
Pavel Fidransky <pavel.fidransky@yoso.fi>
2023-10-03 10:46:43 +0200

    Add Spring Boot

---
+++ bikesharing-rest-api/src/main/java/cz/zcu/kiv/bikesharing/BikesharingApplication.java
@@ -0,0 +1,13 @@
+package cz.zcu.kiv.bikesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikesharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikesharingApplication.class, args);
+    }
+
+}
