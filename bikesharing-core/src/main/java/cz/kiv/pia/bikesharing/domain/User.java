2939d9ccfb081435241a2c912e2a40b8a7c059da
Pavel Fidransky <fidransky@outlook.com>
2023-11-28 10:12:18 +0100

    Implement Spring Security classes in core

--- bikesharing-core/src/main/java/cz/kiv/pia/bikesharing/domain/User.java
+++ bikesharing-core/src/main/java/cz/kiv/pia/bikesharing/domain/User.java
@@ -1,12 +1,17 @@
 package cz.kiv.pia.bikesharing.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
 import java.util.UUID;
 
 /**
  * Represents a single user of the service.
  */
-public class User {
+public class User implements UserDetails {
     /**
      * Unique identifier.
      */
@@ -45,6 +50,41 @@ public class User {
 
     public Role getRole() {
         return role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String getPassword() {
+        return "{noop}password";
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     /**
---
+++ bikesharing-core/src/main/java/cz/kiv/pia/bikesharing/service/DummyUserService.java
@@ -0,0 +1,16 @@
+package cz.kiv.pia.bikesharing.service;
+
+import cz.kiv.pia.bikesharing.domain.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class DummyUserService extends InMemoryUserDetailsManager implements UserService {
+    private static final User user1 = new User(UUID.randomUUID(), "Pavel Fidranský", "pavel.fidransky@yoso.fi", User.Role.REGULAR);
+
+    public DummyUserService() {
+        super(user1);
+    }
+}
