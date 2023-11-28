7088c1fe3e79cb86e6544ea18f03fe9bd6d4e449
Pavel Fidransky <fidransky@outlook.com>
2023-11-28 10:14:16 +0100

    Enable Spring Security in server-side UI

---
+++ bikesharing-ui/src/main/java/cz/kiv/pia/bikesharing/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package cz.kiv.pia.bikesharing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/", "*.css", "*.png").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .logout(withDefaults());
+
+        return http.build();
+    }
+}
