b52a078de37283f649b64197f432322ce66c9694
Pavel Fidransky <fidransky@outlook.com>
2024-10-07 21:01:11 +0200

    Comment out annotation to ignore plain Java servlets, filters and listeners

--- src/main/java/cz/zcu/kiv/pia/labs/Application.java
+++ src/main/java/cz/zcu/kiv/pia/labs/Application.java
@@ -3,15 +3,16 @@ package cz.zcu.kiv.pia.labs;
 import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+//import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 @SpringBootApplication
-@ServletComponentScan
 // this annotation makes Spring Framework scan classpath for plain Java servlets, filters and listeners too
+// commented out so that HelloWorldServlet doesn't conflict with HelloWorldController
+//@ServletComponentScan
 public class Application {
 
     private static final Logger LOG = getLogger(Application.class);
