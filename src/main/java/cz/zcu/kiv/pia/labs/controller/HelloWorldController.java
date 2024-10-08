9edb4905d0f798e31c9dbac20118bb797e357dda
Pavel Fidransky <fidransky@outlook.com>
2024-10-08 11:46:37 +0200

    Make HelloWorldController implement the generated HelloApi interface

--- src/main/java/cz/zcu/kiv/pia/labs/controller/HelloWorldController.java
+++ src/main/java/cz/zcu/kiv/pia/labs/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package cz.zcu.kiv.pia.labs.controller;
 
 import cz.zcu.kiv.pia.labs.service.NumberService;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
-public class HelloWorldController {
+public class HelloWorldController implements HelloApi {
     private final NumberService numberService;
 
     // constructor-based dependency injection
@@ -18,16 +19,15 @@ public class HelloWorldController {
         this.numberService = numberService;
     }
 
-    // create endpoint at http://localhost:8080/hello
-    @GetMapping(path = "/hello", produces = MediaType.TEXT_HTML_VALUE + "; charset=utf-8")
-    public String sayHello(@RequestParam(value = "from", required = false) String from) {
+    @Override
+    public ResponseEntity<String> sayHello(String from) {
         var builder = new StringBuilder("Hello World");
         if (from != null) {
             builder.append(" from ").append(from);
         }
         builder.append("!");
 
-        return "<h1>" + builder + "</h1>";
+        return ResponseEntity.ok("<h1>" + builder + "</h1>");
     }
 
     @GetMapping("/number")
