9b701af9786c19a2deceebb998919bc54fa59c6f
Pavel Fidransky <fidransky@outlook.com>
2024-11-05 08:07:01 +0100

    Send a message to Artemis when new damage is reported

--- pia-labs-core/src/main/java/cz/zcu/kiv/pia/labs/service/MessageListener.java
+++ pia-labs-core/src/main/java/cz/zcu/kiv/pia/labs/service/MessageListener.java
@@ -3,6 +3,7 @@ package cz.zcu.kiv.pia.labs.service;
 import cz.zcu.kiv.pia.labs.event.DamageReportedEvent;
 import org.slf4j.Logger;
 import org.springframework.context.event.EventListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -12,9 +13,11 @@ public class MessageListener {
     private static final Logger LOG = getLogger(MessageListener.class);
 
     private final MessageSender messageSender;
+    private final JmsTemplate jmsTemplate;
 
-    public MessageListener(MessageSender messageSender) {
+    public MessageListener(MessageSender messageSender, JmsTemplate jmsTemplate) {
         this.messageSender = messageSender;
+        this.jmsTemplate = jmsTemplate;
     }
 
     @EventListener
@@ -22,6 +25,8 @@ public class MessageListener {
         LOG.info("Received event about newly reported damage {}", event.damage());
 
         var reportedDamage = event.damage();
-        messageSender.sendReportedDamageMessage(reportedDamage.getImpaired(), reportedDamage.getId());
+        var destination = "kiv.pia.labs.damage.%s".formatted(reportedDamage.getId());
+
+        jmsTemplate.convertAndSend(destination, reportedDamage);
     }
 }
