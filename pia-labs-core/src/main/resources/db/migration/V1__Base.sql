f7b6bab035444fa17a7639d2dfe857eabc0c1692
Pavel Fidransky <fidransky@outlook.com>
2024-12-03 08:05:16 +0100

    Add missing DB structures

--- pia-labs-core/src/main/resources/db/migration/V1__Base.sql
+++ pia-labs-core/src/main/resources/db/migration/V1__Base.sql
@@ -1,10 +1,25 @@
 -- this database migration script is executed by Flyway
 
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` UUID NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `email_address` VARCHAR(255) NOT NULL,
+  `role` TINYINT(1) NOT NULL,
+  PRIMARY KEY `id` (`id`)
+);
+
 CREATE TABLE IF NOT EXISTS `damage` (
   `id` UUID NOT NULL,
+  `insured_user_id` UUID NOT NULL,
+  `impaired_user_id` UUID NOT NULL,
   `timestamp` TIMESTAMP(3) NOT NULL,
   `longitude` DECIMAL(11, 8) NOT NULL,
   `latitude` DECIMAL(10, 8) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
+  `state` TINYINT(1) NOT NULL,
   PRIMARY KEY `id` (`id`)
 );
+
+ALTER TABLE `damage`
+ADD CONSTRAINT `FK_insured_user_id` FOREIGN KEY (`insured_user_id`) REFERENCES `user` (`id`),
+ADD CONSTRAINT `FK_impaired_user_id` FOREIGN KEY (`impaired_user_id`) REFERENCES `user` (`id`);
