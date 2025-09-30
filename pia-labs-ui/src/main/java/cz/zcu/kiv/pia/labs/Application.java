9c9ec9ccd8024e1181e6ac4d726e6e5907ad8027
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-09-30 09:01:09 +0200

    Turn the plain Java app into a Spring Boot app

---
+++ pia-labs-ui/src/main/java/cz/zcu/kiv/pia/labs/Application.java
@@ -0,0 +1,31 @@
+package cz.zcu.kiv.pia.labs;
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+@SpringBootApplication
+// ServletComponentScan annotation makes Spring Framework scan classpath for plain Java servlets, filters and listeners too
+@ServletComponentScan
+public class Application {
+    private static final Logger LOG = getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // prints all registered beans whenever application context is refreshed - most commonly on application startup
+    @EventListener
+    public void onContextRefreshed(ContextRefreshedEvent event) {
+        LOG.info("*************** APPLICATION CONTEXT REFRESHED ***************");
+
+        for (String beanName : event.getApplicationContext().getBeanDefinitionNames()) {
+            LOG.info(beanName);
+        }
+    }
+}
