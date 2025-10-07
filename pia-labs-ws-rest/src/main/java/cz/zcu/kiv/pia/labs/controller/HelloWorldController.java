7fe8e21137a85a380e5aa8f3dc6746b6f6c3337b
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-10-07 11:20:21 +0200

    Create GET /hello API endpoint specs and implement it

---
+++ pia-labs-ws-rest/src/main/java/cz/zcu/kiv/pia/labs/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package cz.zcu.kiv.pia.labs.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1")
+public class HelloWorldController implements HelloApi {
+    @Override
+    public ResponseEntity<String> sayHello(String from) {
+        var builder = new StringBuilder("Hello World");
+        if (from == null) {
+            throw new RuntimeException("The 'from' parameter is missing.");
+        } else {
+            builder.append(" from ").append(from);
+        }
+        builder.append("!");
+
+        return new ResponseEntity<>("<h1>" + builder + "</h1>", new HttpHeaders(), HttpStatus.OK);
+    }
+}
