04ffb6f9efe7a9f0d1e00e2d7d03874f302adde0
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-10-14 08:44:27 +0200

    Add GraphQL dependencies, configure generator and implement hello world query

---
+++ pia-labs-ws-graphql/src/test/java/cz/zcu/kiv/pia/labs/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.zcu.kiv.pia.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
