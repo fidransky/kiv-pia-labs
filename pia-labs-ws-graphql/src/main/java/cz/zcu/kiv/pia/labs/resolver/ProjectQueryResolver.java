174a65cdf1c2f1a8457172a336b92da81716dc6d
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-10-14 12:53:18 +0200

    Demonstrate how BatchMapping works

--- pia-labs-ws-graphql/src/main/java/cz/zcu/kiv/pia/labs/resolver/ProjectQueryResolver.java
+++ pia-labs-ws-graphql/src/main/java/cz/zcu/kiv/pia/labs/resolver/ProjectQueryResolver.java
@@ -1,12 +1,16 @@
 package cz.zcu.kiv.pia.labs.resolver;
 
 import cz.zcu.kiv.pia.labs.graphql.ProjectDTO;
+import cz.zcu.kiv.pia.labs.graphql.UserDTO;
 import cz.zcu.kiv.pia.labs.mapper.ProjectMapper;
 import cz.zcu.kiv.pia.labs.service.ProjectService;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ProjectQueryResolver {
@@ -24,5 +28,11 @@ public class ProjectQueryResolver {
         return projectService.getAllProjects().stream()
                 .map(projectMapper::toDTO)
                 .toList();
+    }
+
+    @BatchMapping(typeName = "ProjectDTO", field = "customer")
+    public Map<ProjectDTO, UserDTO> loadCustomersForProjects(List<ProjectDTO> projects) {
+        // TODO: load customers for given projects in a better optimized way
+        return Collections.emptyMap();
     }
 }
