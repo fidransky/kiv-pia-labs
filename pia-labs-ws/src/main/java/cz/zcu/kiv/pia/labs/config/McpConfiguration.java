edbd61c2b9430e8ad394125352c82fd32751eec0
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-10-20 19:59:27 +0200

    Use specialized ToolCallbackProvider

--- pia-labs-ws/src/main/java/cz/zcu/kiv/pia/labs/config/McpConfiguration.java
+++ pia-labs-ws/src/main/java/cz/zcu/kiv/pia/labs/config/McpConfiguration.java
@@ -3,6 +3,7 @@ package cz.zcu.kiv.pia.labs.config;
 import cz.zcu.kiv.pia.labs.mcp.PiaLabsMcpTools;
 import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +15,8 @@ public class McpConfiguration {
 
     @Bean
     public ToolCallbackProvider toolCallbackProvider(PiaLabsMcpTools tools) {
-        return ToolCallbackProvider.from(ToolCallbacks.from(tools));
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(tools)
+                .build();
     }
 }
