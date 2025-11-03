c7df6b4fa213a35a97a9a12d76e8d085dfa95d72
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-11-03 14:37:21 +0100

    Implement basic error handling

--- pia-labs-ui/src/main/java/cz/zcu/kiv/pia/labs/controller/advice/GlobalControllerAdvice.java
+++ pia-labs-ui/src/main/java/cz/zcu/kiv/pia/labs/controller/advice/GlobalControllerAdvice.java
@@ -1,8 +1,14 @@
 package cz.zcu.kiv.pia.labs.controller.advice;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
@@ -10,5 +16,15 @@ public class GlobalControllerAdvice {
     @ModelAttribute("request")
     public HttpServletRequest addRequestToModel(HttpServletRequest request) {
         return request;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleException(Exception ex, HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView("error");
+
+        modelAndView.addObject("message", ex.getMessage() != null ? ex.getMessage() : "An unexpected error occurred");
+        modelAndView.addObject("request", request);
+
+        return modelAndView;
     }
 }
