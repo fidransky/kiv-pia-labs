16e75e3e2533bebaf07a8069ab1e2304b8142ad7
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-11-03 08:56:53 +0100

    Reimplement Hello World screen using Thymeleaf template engine

--- pia-labs-ui/src/main/java/cz/zcu/kiv/pia/labs/controller/HelloWorldController.java
+++ pia-labs-ui/src/main/java/cz/zcu/kiv/pia/labs/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,8 +19,11 @@ public class HelloWorldController {
 
     // create endpoint at http://localhost:8080/hello
     @GetMapping(path = "/hello", produces = MediaType.TEXT_HTML_VALUE + "; charset=utf-8")
-    @ResponseBody
-    public String sayHello(@RequestParam(value = "from", required = false) String from) {
+    public String sayHello(
+            @RequestParam(value = "from", required = false) String from,
+            Model model
+    ) {
+        // build resulting message
         var builder = new StringBuilder("Hello World");
         if (from == null) {
             throw new RuntimeException("The 'from' parameter is missing.");
@@ -28,7 +32,12 @@ public class HelloWorldController {
         }
         builder.append("!");
 
-        return "<h1>" + builder + "</h1>";
+        var message = builder.toString();
+
+        // add message to model
+        model.addAttribute("message", message);
+
+        return "greeting";
     }
 
     // create endpoint at http://localhost:8080/number
