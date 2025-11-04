a1173dce5beed792efc91f406034d4057d526325
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-11-04 08:48:34 +0100

    Configure CORS to allow loading data from React app origin

---
+++ pia-labs-ws-rest/src/main/java/cz/zcu/kiv/pia/labs/CORSConfiguration.java
@@ -0,0 +1,15 @@
+package cz.zcu.kiv.pia.labs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173");
+    }
+}
