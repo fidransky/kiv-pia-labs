d3ec9dd4acbabc855b75c8f53da12590c679c0f7
Pavel Fidransky <pavel.fidransky@yoso.fi>
2025-11-28 18:06:21 +0100

    Configure security in UI module

---
+++ pia-labs-ui/src/main/java/cz/zcu/kiv/pia/labs/configuration/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package cz.zcu.kiv.pia.labs.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout(LogoutConfigurer::permitAll)
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/hello").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
